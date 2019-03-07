de162efe275da9383d33d9d68af84c22e96174c9
jefferson <cardbeatt@gmail.com>
2019-03-07 16:14:26 -0300

    added vue and vue router

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  cors = require("cors");
+
+const app = express();
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+app.use(express.static("dist"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("index");
+});
---
+++ dist/app.4c9f089ed2a8888cf07e.hot-update.js
@@ -0,0 +1,242 @@
+webpackHotUpdate("app",{
+
+/***/ "./node_modules/core-js/modules/_a-function.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_add-to-unscopables.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_an-instance.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_an-object.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_array-includes.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_classof.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_cof.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_core.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_ctx.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_defined.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_descriptors.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_dom-create.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_enum-bug-keys.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_export.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_fails.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_for-of.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_function-to-string.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_global.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_has.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_hide.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_html.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_ie8-dom-define.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_invoke.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_iobject.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_is-array-iter.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_is-object.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_iter-call.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_iter-create.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_iter-define.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_iter-detect.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_iter-step.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_iterators.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_library.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_microtask.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_new-promise-capability.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_object-assign.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_object-create.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_object-dp.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_object-dps.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_object-gops.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_object-gpo.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_object-keys-internal.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_object-keys.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_object-pie.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_perform.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_promise-resolve.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_property-desc.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_redefine-all.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_redefine.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_set-species.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_set-to-string-tag.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_shared-key.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_shared.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_species-constructor.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_task.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_to-absolute-index.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_to-integer.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_to-iobject.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_to-length.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_to-object.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_to-primitive.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_uid.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_user-agent.js":
+false,
+
+/***/ "./node_modules/core-js/modules/_wks.js":
+false,
+
+/***/ "./node_modules/core-js/modules/core.get-iterator-method.js":
+false,
+
+/***/ "./node_modules/core-js/modules/es6.array.iterator.js":
+/*!************************************************************!*\
+  !*** ./node_modules/core-js/modules/es6.array.iterator.js ***!
+  \************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or directory, open '/home/jefferson/Documents/projects/book-trading-club/node_modules/core-js/modules/es6.array.iterator.js'\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9ub2RlX21vZHVsZXMvY29yZS1qcy9tb2R1bGVzL2VzNi5hcnJheS5pdGVyYXRvci5qcy5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./node_modules/core-js/modules/es6.array.iterator.js\n");
+
+/***/ }),
+
+/***/ "./node_modules/core-js/modules/es6.object.assign.js":
+/*!***********************************************************!*\
+  !*** ./node_modules/core-js/modules/es6.object.assign.js ***!
+  \***********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or directory, open '/home/jefferson/Documents/projects/book-trading-club/node_modules/core-js/modules/es6.object.assign.js'\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9ub2RlX21vZHVsZXMvY29yZS1qcy9tb2R1bGVzL2VzNi5vYmplY3QuYXNzaWduLmpzLmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./node_modules/core-js/modules/es6.object.assign.js\n");
+
+/***/ }),
+
+/***/ "./node_modules/core-js/modules/es6.promise.js":
+/*!*****************************************************!*\
+  !*** ./node_modules/core-js/modules/es6.promise.js ***!
+  \*****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or directory, open '/home/jefferson/Documents/projects/book-trading-club/node_modules/core-js/modules/es6.promise.js'\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9ub2RlX21vZHVsZXMvY29yZS1qcy9tb2R1bGVzL2VzNi5wcm9taXNlLmpzLmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./node_modules/core-js/modules/es6.promise.js\n");
+
+/***/ }),
+
+/***/ "./node_modules/core-js/modules/es7.promise.finally.js":
+/*!*************************************************************!*\
+  !*** ./node_modules/core-js/modules/es7.promise.finally.js ***!
+  \*************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or directory, open '/home/jefferson/Documents/projects/book-trading-club/node_modules/core-js/modules/es7.promise.finally.js'\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9ub2RlX21vZHVsZXMvY29yZS1qcy9tb2R1bGVzL2VzNy5wcm9taXNlLmZpbmFsbHkuanMuanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiIiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./node_modules/core-js/modules/es7.promise.finally.js\n");
+
+/***/ })
+
+})
