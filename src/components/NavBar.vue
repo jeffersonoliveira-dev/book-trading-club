f62c58a61c9baccba17371b8c2b8f10d673bfeb7
jefferson <cardbeatt@gmail.com>
2019-03-08 19:19:17 -0300

    added header and routs

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,30 @@
-<template> </template>
+<template>
+<div class="top">
+    <nav>
+    <div class="nav-wrapper light-blue lighten-4">
+      <div class="left">
+          <router-link to="/"> Home </router-link>
+      </div>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><router-link to="/login"> Login </router-link></li>
+      </ul>
+    </div>
+  </nav>
+</div>
+</template>
 
 <script>
-export default {};
+export default {
+
+};
 </script>
 
-<style></style>
+<style>
+.top {
+  overflow: hidden;
+  background-color: #333;
+  position: absolute; /* Set the navbar to fixed position */
+  top: 0; /* Position the navbar at the top of the page */
+  width: 100%; /* Full width */
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1> HEHE AQUI É O LOGIN VIADO </h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
