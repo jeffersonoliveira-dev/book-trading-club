5e83c190c4beada50f86b73a9e26470168603b11
jefferson <cardbeatt@gmail.com>
2019-04-20 22:31:15 -0300

    pseudo code un commentary to get things started

--- app/src/routes/Login.js
+++ app/src/routes/Login.js
@@ -10,11 +10,21 @@ export default class Login extends Component {
   };
   uiConfig = {
     signInFlow: 'popup',
-    signInSuccessUrl: '/signedIn',
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        console.log(authResult.user.uid);
+        // create ref with authResult.user.uid and then call real time db
+        // problably making this a component to user at the login or get rid of the sign up maybe?
+        // if account doesnt exist before, after checking user.uid > redirect to profile settings where the city and state will be chosen
+        // else > dashboard
+
+        return true;
+      },
+    },
   };
 
   componentWillUnmount() {
