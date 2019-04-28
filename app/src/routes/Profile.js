30b29c55bb184ec1abffc87ac96a54557ace3f38
jefferson <cardbeatt@gmail.com>
2019-04-27 22:38:18 -0300

    form started on profile

--- app/src/routes/Profile.js
+++ app/src/routes/Profile.js
@@ -1,8 +1,33 @@
 import React, {Component} from 'react';
+import TextField from '@material-ui/core/TextField';
 
 class Profile extends Component {
   render() {
-    return <div> this is the profile page</div>;
+    return (
+      <form autoComplete="off">
+        <TextField
+          id="name"
+          label="name"
+          value=""
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          id="city"
+          label="city"
+          value=""
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          id="state"
+          label="state"
+          value=""
+          margin="normal"
+          variant="outlined"
+        />
+      </form>
+    );
   }
 }
 
