0f313fcaf79cd964952505ee268ceb514e4c0025
jefferson <cardbeatt@gmail.com>
2019-04-29 14:25:35 -0300

    importing book to start the CRUD functionality

--- app/src/routes/Books.js
+++ app/src/routes/Books.js
@@ -1,4 +1,11 @@
 import React, {Component} from 'react';
+import database from '../firebase';
+import {connect} from 'react-redux';
+import Book from '../components/Books/Book';
+
+const mapStateToProps = state => ({
+  books: state.books,
+});
 
 class Books extends Component {
   render() {
