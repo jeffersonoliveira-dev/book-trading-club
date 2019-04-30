8cd60ba9137de90918bf6cbed74709ed3bb3c4b6
jefferson <cardbeatt@gmail.com>
2019-04-30 12:53:33 -0300

    updated adduser

--- app/src/redux/actions/addUser.js
+++ app/src/redux/actions/addUser.js
@@ -2,6 +2,9 @@ import {ADD_USER} from './actionTypes';
 
 export const addUser = payload => ({
   type: ADD_USER,
+  auth: true,
   userData: payload.userData,
   userToken: payload.userToken,
+  trades: payload.userData.trades,
+  books: payload.userData.books,
 });
