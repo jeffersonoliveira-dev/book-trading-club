69a3ceb9b4d9203a730882dee4ada986ff6891f7
jefferson <cardbeatt@gmail.com>
2019-05-01 18:30:50 -0300

    changed Grid to Book and created delete function

--- app/src/components/Books/Book.js
+++ app/src/components/Books/Book.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import Fab from '@material-ui/core/Fab';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Paper from '@material-ui/core/Paper';
+import {updateBooks} from '../../redux/actions/updateBooks';
+import styles from '../../components/Books/styles/Book';
+import {withStyles} from '@material-ui/core/styles';
+import {connect} from 'react-redux';
+import database from '../../firebase';
+
+const mapStateToProps = state => ({
+  books: state.books,
+  userToken: state.userToken,
+});
 
 class Book extends React.Component {
+  handleDelete = () => {
+    let newBooks = this.props.books.filter(book => {
+      return book !== this.props.name;
+    });
+    database
+      .collection('users')
+      .doc(this.props.userToken)
+      .update({books: newBooks})
+      .then(() => {
+        this.props.updateBooks(newBooks);
+      });
+  };
+
   render() {
+    const {classes} = this.props;
     return (
       <div>
-        <div>{this.props.name}</div>
+        <Paper className={classes.item}>
+          <IconButton
+            aria-label="Delete"
+            onClick={this.handleDelete}
+            className={classes.delete}>
+            <DeleteIcon fontSize="small" />
+          </IconButton>
+          <div>
+            <h4>{this.props.name}</h4>
+          </div>
+        </Paper>
       </div>
     );
   }
@@ -12,4 +50,7 @@ class Book extends React.Component {
 
 // name , username,
 
-export default Book;
+export default connect(
+  mapStateToProps,
+  {updateBooks},
+)(withStyles(styles)(Book));
