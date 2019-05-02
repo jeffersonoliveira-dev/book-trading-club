c8c0be221726fc0205a6004c00b5ba7258675236
jefferson <cardbeatt@gmail.com>
2019-05-02 14:35:18 -0300

    rendering all books from all users

--- app/src/routes/Dashboard.js
+++ app/src/routes/Dashboard.js
@@ -1,15 +1,57 @@
 import React, {Component} from 'react';
+import styles from '../components/Books/styles/Book';
+import {withStyles} from '@material-ui/core/styles';
 import database from '../firebase';
+import Grid from '@material-ui/core/Grid';
+import {connect} from 'react-redux';
+import Book from '../components/Books/Book';
 
-// real time here
-// get all books from database with snapshots
-// rendering components now
 class Dashboard extends Component {
-  componentWillMount() {}
+  constructor(props) {
+    super();
+    this.state = {
+      allBooks: [],
+    };
+  }
+  componentWillMount() {
+    database.collection('users').onSnapshot(snap => {
+      // get notification if this changes
+      let allBooks = [];
+      snap.forEach(item => {
+        let token = item._key.path.segments.pop();
+        this.setState({
+          allBooks: [
+            ...this.state.allBooks,
+            {token: token, books: item.data().books, name: item.data().name},
+          ],
+        });
+      });
+      console.log(this.state.allBooks);
+    });
+  }
 
   render() {
-    return <div>this is the dashboard</div>;
+    const {classes} = this.props;
+
+    let Books = this.state.allBooks.map((user, index) => {
+      let userBooks = user.books.map((book, index) => {
+        return (
+          <Grid item xs key={index}>
+            <Book name={book} key={index} />
+          </Grid>
+        );
+      });
+      return userBooks;
+    });
+    console.log(Books);
+    return (
+      <div className={classes.container}>
+        <Grid container spacing={24}>
+          {Books}
+        </Grid>
+      </div>
+    );
   }
 }
 
-export default Dashboard;
+export default connect()(withStyles(styles)(Dashboard));
