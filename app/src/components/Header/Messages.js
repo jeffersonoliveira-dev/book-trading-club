57a7b8f85607eb94cb018a058a37b6dfbe917117
jefferson <cardbeatt@gmail.com>
2019-05-20 19:42:19 -0300

    added link to messages page

--- app/src/components/Header/Messages.js
+++ app/src/components/Header/Messages.js
@@ -2,21 +2,22 @@ import React, {Component} from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import MailIcon from '@material-ui/icons/Mail';
 import Badge from '@material-ui/core/Badge';
+import {Link} from 'react-router-dom';
 
 class Messages extends Component {
   render() {
     return (
       <IconButton color="inherit">
-        <Badge badgeContent={4} color="secondary">
-          <MailIcon />
+        <Badge badgeContent={0} color="secondary">
+          <Link to="/messages">
+            <MailIcon />
+          </Link>
         </Badge>
       </IconButton>
     );
   }
 }
 // trade box here
-
-// badgeContent = aos trades =>
-// fazer algo parecido com o profile pras trades aparecerem com os botoes de aceitar // negar
+// classes here
 
 export default Messages;
