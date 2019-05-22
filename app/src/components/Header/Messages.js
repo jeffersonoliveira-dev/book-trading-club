e388cffd21f3455d9774a9ce8faa57971afc16f3
jefferson <cardbeatt@gmail.com>
2019-05-22 15:34:57 -0300

    deleted Link and push to history on click

--- app/src/components/Header/Messages.js
+++ app/src/components/Header/Messages.js
@@ -1,17 +1,44 @@
 import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton';
 import MailIcon from '@material-ui/icons/Mail';
 import Badge from '@material-ui/core/Badge';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {withStyles} from '@material-ui/core/styles';
+import styles from './styles/index';
+
+const mapStateToProps = state => ({
+  trades: state.trades,
+});
 
 class Messages extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      counter: 0,
+    };
+  }
+  componentWillMount() {
+    let counter = 0;
+    this.props.trades.map(trade => {
+      if (trade.read === false) {
+        counter = counter + 1;
+      }
+    });
+    this.setState({
+      counter: counter,
+    });
+  }
   render() {
+    const {classes} = this.props;
     return (
       <IconButton color="inherit">
-        <Badge badgeContent={0} color="secondary">
-          <Link to="/messages">
-            <MailIcon />
-          </Link>
+        <Badge
+          badgeContent={this.state.counter}
+          color="secondary"
+          onClick={() => this.props.history.push('/messages')}>
+          <MailIcon />
         </Badge>
       </IconButton>
     );
@@ -20,4 +47,6 @@ class Messages extends Component {
 // trade box here
 // classes here
 
-export default Messages;
+export default connect(mapStateToProps)(
+  withRouter(withStyles(styles)(Messages)),
+);
