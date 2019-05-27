ae4f2ad567da54219102dd5cbc67122423d4178a
jefferson oliveira <cardbeatt@gmail.com>
2019-05-27 17:32:38 -0300

    modified tradebox

--- app/src/components/Messages/TradeBox.js
+++ app/src/components/Messages/TradeBox.js
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
+import 
 
 class TradeBox extends Component {
   render() {
-    return <h1>trade component</h1>;
+    return (
+      <div className="flex">
+      <div className="item">
+         <div className="tradeBox">
+              <p> Fulano wants to trade his book called <br/>name of the book <br/> for your <br/> name of the book</p>
+            <hr/>
+            <div className="buttons">
+               <div className="button"> 
+                  <a className="accept" href="#">accept</a>
+               </div>
+               <div className="button"> 
+                  <a className="reject" href="#">reject</a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+    )
   }
 }
 
