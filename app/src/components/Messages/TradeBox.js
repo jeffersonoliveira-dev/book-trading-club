8be4bdae240622cd7c7f3872b89f99b67f0c5b9f
jefferson oliveira <cardbeatt@gmail.com>
2019-05-27 17:42:34 -0300

    added css styles to tradebox

--- app/src/components/Messages/TradeBox.js
+++ app/src/components/Messages/TradeBox.js
@@ -1,20 +1,20 @@
 import React, {Component} from 'react';
-import 
+import styles from './styles/index'
 
 class TradeBox extends Component {
   render() {
     return (
-      <div className="flex">
-      <div className="item">
-         <div className="tradeBox">
+      <div className={styles.flex}>
+      <div className={styles.item}>
+         <div>
               <p> Fulano wants to trade his book called <br/>name of the book <br/> for your <br/> name of the book</p>
             <hr/>
-            <div className="buttons">
-               <div className="button"> 
-                  <a className="accept" href="#">accept</a>
+            <div className={styles.buttons}>
+               <div className={styles.button}> 
+                  <a className={styles.accept} href="#">accept</a>
                </div>
-               <div className="button"> 
-                  <a className="reject" href="#">reject</a>
+               <div className={styles.button}> 
+                  <a className={styles.reject} href="#">reject</a>
                </div>
             </div>
          </div>
--- app/src/components/Messages/styles/index.js
+++ app/src/components/Messages/styles/index.js
@@ -1,3 +1,43 @@
-const styles = {};
+const styles = {
+    flex: {
+        display: "flex",
+        margin: "0 auto",
+        maxWidth: "360px",
+        border: "1px solid #ccc"
+    },
+    item: {
+        flex: 1,
+        padding: "0 20px",
+        textAlign: 'center',
+    },
+    buttons: {
+        display: 'flex',
+        justifyContent: "space-between",
+        padding: '10px'
+
+    },
+    button: {
+        margin: 'auto',
+        padding: '10px'
+    },
+    hr: {
+        width: '100%'
+    },
+    a: {
+        textDecoration: 'none',
+        color: 'white';
+        fontWeight: 600;
+        padding: "10px";
+        paddingRight: "20px";
+        paddingLeft: "20px";
+    },
+    accept: {
+        backgroundColor: 'skyblue'
+    },
+    reject: {
+        backgroundColor: 'red'
+    }
+
+};
 
 export default styles;
