44690ac229015778fa13fe362033ddc55a76d0b6
jefferson <cardbeatt@gmail.com>
2019-05-28 14:27:23 -0300

    added design with styled components

--- app/src/components/Messages/TradeBox.js
+++ app/src/components/Messages/TradeBox.js
@@ -1,26 +1,89 @@
 import React, {Component} from 'react';
-import styles from './styles/index'
+import styled from 'styled-components';
+import database from '../../firebase';
+
+const Container = styled.div`
+  position: relative;
+  margin-top: 50px;
+`;
+
+const Flex = styled.div`
+  display: flex;
+  margin: 0 auto;
+  max-width: 30%;
+  min-width: 260px;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+const Item = styled.div`
+  flex: 1;
+  border: 1px solid #ccc;
+  text-align: center;
+  margin: 10px;
+  padding: 20px;
+  box-shadow: 0 8px 6px -6px black;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+`;
+
+const Button = styled.a`
+  text-decoration: none;
+  color: white;
+  font-weight: 600;
+  padding: 10px;
+  padding-right: 20px;
+  padding-left: 20px;
+`;
+
+const AcceptButton = styled(Button)`
+  background-color: skyblue;
+`;
+const RejectButton = styled(Button)`
+  background-color: red;
+`;
 
 class TradeBox extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      name: '',
+    };
+  }
+
+  componentWillMount() {
+    database
+      .collection('users')
+      .doc(this.props.trade.userOffer)
+      .get()
+      .then(doc => this.setState({name: doc.data().name}));
+  }
+
   render() {
+    console.log(this.props);
     return (
-      <div className={styles.flex}>
-      <div className={styles.item}>
-         <div>
-              <p> Fulano wants to trade his book called <br/>name of the book <br/> for your <br/> name of the book</p>
-            <hr/>
-            <div className={styles.buttons}>
-               <div className={styles.button}> 
-                  <a className={styles.accept} href="#">accept</a>
-               </div>
-               <div className={styles.button}> 
-                  <a className={styles.reject} href="#">reject</a>
-               </div>
-            </div>
-         </div>
-      </div>
-   </div>
-    )
+      <Container>
+        <Flex>
+          <Item>
+            {this.state.name} wants to trade his
+            <br />
+            {this.props.trade.bookOffer} <br />
+            for yours
+            <br />
+            {this.props.trade.bookWish}
+            <hr />
+            <Buttons>
+              <AcceptButton href="#">accept</AcceptButton>
+              <RejectButton href="#">reject</RejectButton>
+            </Buttons>
+          </Item>
+        </Flex>
+      </Container>
+    );
   }
 }
 
