1101a0bdbd42da38eb7cd78aa9134cd0e8b2f40c
jefferson oliveira <cardbeatt@gmail.com>
2019-08-27 18:05:56 -0300

    revamping

---
+++ src/secret/config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: 'AIzaSyC6KHdYXZQ0geVJ69sDGjNynpOBPrHu1cY',
+  authDomain: 'book-trading-club-21fee.firebaseapp.com',
+  databaseURL: 'https://book-trading-club-21fee.firebaseio.com',
+  projectId: 'book-trading-club-21fee',
+  storageBucket: 'book-trading-club-21fee.appspot.com',
+  messagingSenderId: '972411479567',
+  appId: '1:972411479567:web:df0c5d31fc433dea',
+};
+
+export default config;
