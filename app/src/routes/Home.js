599618322d26c71e14f73049c979d911e539dc9b
jefferson oliveira <cardbeatt@gmail.com>
2019-08-27 17:13:05 -0300

    finished

--- app/src/routes/Home.js
+++ app/src/routes/Home.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 3px 3px 5px 6px #ccc;
+  padding: 30px;
+`;
+
+const Title = styled.h1`
+  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+  font-family: 'Roboto', sans-serif;
+`;
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="container">
-        <p>this is the home page </p>
-      </div>
+      <Container>
+        <Title>Welcome to the book trading club</Title>
+      </Container>
     );
   }
 }
